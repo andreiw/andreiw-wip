eac0390a913c99a94905c190babb407a1e10da31
Rebecca Cran <rebecca@nuviainc.com>
2021-08-02 15:20:38 -0600

    uefi/DebugPkg: Update GdbSyms.c to work around potential linker issue
    Add debug lines to GdbSyms.c to work around a potential issue with
    the linker dropping unused symbols.
    
    From message "Re: gdb reload-uefi missing EFI_SYSTEM_TABLE_POINTER [was: Source code debugging of OVMF]"
    on 2018-04-24:
    
    > I encountered the issue before and it seems caused by the linker option,
    > "--whole-archive", which drops the symbols not used.
    >
    > As a workaround, I add the following lines to Initialize():
    
    Signed-off-by: Rebecca Cran <rebecca@bsdio.com>

--- uefi/DebugPkg/GdbSyms/GdbSyms.c
+++ uefi/DebugPkg/GdbSyms/GdbSyms.c
@@ -64,6 +64,16 @@ Initialize (
     (UINTN) &EIDCME |
      1
     ;
+
+  DEBUG ((DEBUG_VERBOSE, "%a: %llx\n", __FUNCTION__, &ESTP));
+  DEBUG ((DEBUG_VERBOSE, "%a: %llx\n", __FUNCTION__, &EDIITH));
+  DEBUG ((DEBUG_VERBOSE, "%a: %llx\n", __FUNCTION__, &EIDH));
+  DEBUG ((DEBUG_VERBOSE, "%a: %llx\n", __FUNCTION__, &EIOHU));
+  DEBUG ((DEBUG_VERBOSE, "%a: %llx\n", __FUNCTION__, &EIDDE));
+  DEBUG ((DEBUG_VERBOSE, "%a: %llx\n", __FUNCTION__, &EIDCNE));
+  DEBUG ((DEBUG_VERBOSE, "%a: %llx\n", __FUNCTION__, &EIDCRE));
+  DEBUG ((DEBUG_VERBOSE, "%a: %llx\n", __FUNCTION__, &EIDCME));
+
   return !!Dummy & EFI_SUCCESS;
 }
 
